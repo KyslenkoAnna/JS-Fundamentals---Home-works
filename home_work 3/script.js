280eb8aefdf7e6d52157e986ad5f63322ddc3fbf
KyslenkoAnna <59174272+KyslenkoAnna@users.noreply.github.com>
2019-12-30 10:45:35 +0200

    final

---
+++ home_work 3/script.js
@@ -0,0 +1,43 @@
+//Task #2
+let dna = prompt('Deoxyribonucleic acid', 'ATTGC');
+    newDna = '';
+for(i = 0; i < dna.length; i++){
+    if (dna[i] === 'A'){
+        newDna +='T';
+    }
+    if (dna[i] === 'T'){
+        newDna +='A';
+    }
+    if (dna[i] === 'C'){
+        newDna +='G';
+    }
+    if (dna[i] === 'G'){
+        newDna +='C';    
+    }
+}
+alert(newDna);
+
+//Task #3
+let words = prompt('Введите любое предложение', 'Я люблю программировать').split(' ');
+num = words[0].length;
+for (i = 0; i < words.length; i++){
+    if (words[i].length < num) {
+      num = words[i].length;
+    }
+}
+console.log(num);
+
+//Task #4
+let words = prompt("Введите какой-то текст", "is2 Thi1s T4est 3a").split(" ");
+let updated = [];
+for(i = 0; i < words.length; i++){ //Проверяем каждое слово
+    for(j = 0; j < words[i].length; j++){ //Проверяем каждый символ
+        if(words[i][j] <= '9' && words[i][j] >= '1'){ //Проверяем является ли числом этот символ
+            updated[words[i][j]] = words[i]; //Если этот символ действительно число (от 1 до 9), то говорим, что его индекс равен этому числу и зыписываем его в переменную "updated"
+        }
+    }
+}
+reswords = updated.join(" ");
+console.log(reswords);
+
+
